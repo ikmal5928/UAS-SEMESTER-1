ca55930e222d79002156b3804f33169dd2e82c9e
ikmal5928 <148456816+ikmal5928@users.noreply.github.com>
2024-01-25 22:22:38 +0700

    Add files via upload

---
+++ jawaban7.java
@@ -0,0 +1,19 @@
+
+public class Jawaban7 {
+
+	public static void main (String[] args) {
+		int n = 5;
+		int i, j;
+		for (i = 0; i < n ; i++) {
+			for (j = n; j > i; j--) {
+				System.out.print(" ");
+				System.out.print(" ");
+			}
+			for (j = 0; j <= i; j++) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
